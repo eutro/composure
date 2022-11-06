5b7fd826e1761220b970e4adfda176d1c2852212
eutro <benedek.szilvasy@gmail.com>
2022-11-06 09:40:07 +0000

    .

--- build.sh
+++ build.sh
@@ -3,14 +3,14 @@
 HERE="$(dirname "$(readlink -f $0)")"
 cd "$HERE/project"
 
-rm -r ./export/
-mkdir ./export/
+rm -r ../export/
+mkdir ../export/
 
 godot --export "Linux/X11"
 
 godot --export "Windows Desktop"
 
-mkdir -p ./export/HTML5/
+mkdir -p ../export/HTML5/
 # sed one-liner my beloved
 sed -si '/\[rendering\]/{$!{N;s#\[rendering\]\n#\0\nquality/driver/driver_name="GLES2"#;ty;P;D;:y}}' ./project.godot
 godot --export "HTML5"
